b69c5b4883a37fcd98086db115d694f96a169b5d
Alen Gomez <81427082+alenpragma@users.noreply.github.com>
2023-07-09 18:00:57 +0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
             className='w-[266px] h-[172px] object-contain'
           />
           <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
-            A new way to make the payments easy, reliable and secure.         
+            We are a team to one of the fastest-growing communities at the intersection of variety gaming, e-sports, and Blockchain.         
           </p>
         </div>
         <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
