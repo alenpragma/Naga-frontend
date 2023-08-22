715edb337ad22be216a01337fcf2c3d0feefcd44
Remon Tripura <remontripura045@gmail.com>
2023-08-22 15:00:24 +0600

    color changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import { aboutUs } from "../assets";
 import { features, ourProjectData } from "../constants";
+import Heading from "../shared/Heading";
 import styles from "../style";
 import { TiTick } from "react-icons/ti";
 
@@ -7,7 +8,10 @@ import { TiTick } from "react-icons/ti";
 const About = () => {
     return (
         <section className="text-white">
-            <div className="lg:flex justify-center items-center my-20">
+            <div>
+                <Heading heading="About Us" title="Nagatrade About Us"/>
+            </div>
+            <div className="lg:flex justify-center items-center my-20 max-w-7xl mx-auto">
                 <div className="lg:w-1/2">
                     <img className="w-full p-10" src={aboutUs} alt="" />
                 </div>
@@ -17,30 +21,32 @@ const About = () => {
                     <div>
                         <h2 className="font-semibold"> Nagatrade Upcoming Projects:</h2>
                         <ul>
-                            <li className="flex items-center"><TiTick className="mr-3"/> Naga Hospital</li>
-                            <li className="flex items-center"><TiTick className="mr-3"/> Naga Real Estate</li>
-                            <li className="flex items-center"><TiTick className="mr-3"/> Naga Car Factory</li>
-                            <li className="flex items-center"><TiTick className="mr-3"/> Naga Agricultural Farm</li>
-                            <li className="flex items-center"><TiTick className="mr-3"/> Naga Crypto and Forex</li>
-                            <li className="flex items-center"><TiTick className="mr-3"/> Naga Hotel and Restaurant</li>
-                            <li className="flex items-center"><TiTick className="mr-3"/> Naga Technologies</li>
-                            <li className="flex items-center"><TiTick className="mr-3"/> Naga Trading (Export & Import)</li>
+                            <li className="flex items-center"><TiTick className="mr-3" /> Naga Hospital</li>
+                            <li className="flex items-center"><TiTick className="mr-3" /> Naga Real Estate</li>
+                            <li className="flex items-center"><TiTick className="mr-3" /> Naga Car Factory</li>
+                            <li className="flex items-center"><TiTick className="mr-3" /> Naga Agricultural Farm</li>
+                            <li className="flex items-center"><TiTick className="mr-3" /> Naga Crypto and Forex</li>
+                            <li className="flex items-center"><TiTick className="mr-3" /> Naga Hotel and Restaurant</li>
+                            <li className="flex items-center"><TiTick className="mr-3" /> Naga Technologies</li>
+                            <li className="flex items-center"><TiTick className="mr-3" /> Naga Trading (Export & Import)</li>
                         </ul>
                     </div>
                 </div>
             </div>
-            <div>
+            <div className="max-w-7xl mx-auto">
                 <div className='flex-1 flex flex-col'>
                     <h2 className={styles.heading3}>Our Project</h2>
                     <p className={`${styles.paragraph3} mt-5`}>
                         Check On Our Project
                     </p>
                 </div>
-                <div className="lg:grid grid-cols-3 gap-10">
+                <div className="grid lg:grid-cols-3 sm:grid-cols-2  gap-5">
                     {
                         ourProjectData.map(data => <div className="bg-slate-200 rounded h-[500px] shadow-xl" key={data.id}>
+                            <div className="flex justify-center items-center">
                             <img className="w-full p-5" src={aboutUs} alt="" />
-                            <div className="card-body">
+                            </div>
+                            <div className="p-5">
                                 <h2 className="card-title text-black">{data.title}</h2>
                                 <p className="text-black">{data.description}</p>
                             </div>
@@ -48,30 +54,30 @@ const About = () => {
                     }
                 </div>
             </div>
-            <div className="mt-20">
+            <div className="mt-20 max-w-7xl mx-auto">
                 <div className='flex-1 flex flex-col'>
                     <h2 className={styles.heading3}>Why Choose Us</h2>
-                    <p className={`${styles.paragraph3} mt-5`}>
+                    <p className={`${styles.paragraph3} mt-5 mb-5`}>
                         Check On Our Project
                     </p>
                 </div>
 
-                <div className="lg:grid grid-cols-3 gap-10">
+                <div className="grid lg:grid-cols-3 sm:grid-cols-2 gap-10 ">
                     {
                         features.map(data => <div className="rounded-md h-auto shadow-xl lg:mt-0 mt-5" key={data.id}>
                             <div className="relative">
-                                <div className="flex rounded-t-md justify-center items-center h-[100px] bg-red-300">
+                                <div className="flex rounded-t-md justify-center items-center h-[100px] bg-[#643666]">
 
                                 </div>
-                                <div className="card-body rounded-b-md h-[200px] bg-green-300 py-20">
-                                    <h2 className="card-title text-black">{data.title}</h2>
-                                    <p className="text-black">{data.content}</p>
+                                <div className="card-body rounded-b-md h-[250px] bg-[#1E2038] py-20 text-center">
+                                    <h2 className="text-[18px] font-bold">{data.title}</h2>
+                                    <p className="">{data.content}</p>
                                 </div>
-                                <div className="top-10 left-[160px] absolute">
-                                    <img className="w-28 p-3 border-4 border-white rounded-full bg-white" src={data.icon} alt="" />
-                                </div>
+                                <div className="absolute top-10 left-0 right-0 w-28 mx-auto">
+                                <img className="p-3 border-4 border-white rounded-full bg-white" src={data.icon} alt="" />
                             </div>
-
+                            </div>
+                           
                         </div>)
                     }
                 </div>
@@ -81,3 +87,6 @@ const About = () => {
 };
 
 export default About;
+
+
+
---
+++ src/shared/Heading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Heading = ({heading, title}) => {
+    return (
+        <div className='pt-36 pb-24 text-center max-w-[300px] mx-auto'>
+            <h2 className='text-6xl text-white border-b-2 border-white pb-2 font-bold'>{heading}</h2>
+            <p className='text-white font-bold mt-2'>{title}</p>
+        </div>
+    );
+};
+
+export default Heading;
